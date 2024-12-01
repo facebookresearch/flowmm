73230b3f214d7a522a90400749abdbb8209b1cc8
mrx <mrx@mit.edu>
2024-12-01 11:00:08 -0500

    MAINT: Copied over over integration steps to solve partial import error

--- src/rfm_docking/manifm/solvers.py
+++ src/rfm_docking/manifm/solvers.py
@@ -3,7 +3,37 @@
 import torch
 from tqdm import tqdm
 from manifm.manifolds import Euclidean
-from manifm.solvers import euler_step, midpoint_step, rk4_step
+# from manifm.solvers import euler_step, midpoint_step, rk4_step
+
+def euler_step(odefunc, xt, vt, t0, dt, manifold=None):
+    if manifold is not None:
+        return manifold.expmap(xt, dt * vt)
+    else:
+        return xt + dt * vt
+
+
+def midpoint_step(odefunc, xt, vt, t0, dt, manifold=None):
+    half_dt = 0.5 * dt
+    if manifold is not None:
+        x_mid = xt + half_dt * vt
+        v_mid = odefunc(t0 + half_dt, x_mid)
+        v_mid = manifold.transp(x_mid, xt, v_mid)
+        return manifold.expmap(xt, dt * v_mid)
+    else:
+        x_mid = xt + half_dt * vt
+        return xt + dt * odefunc(t0 + half_dt, x_mid)
+
+
+def rk4_step(odefunc, xt, vt, t0, dt, manifold=None):
+    k1 = vt
+    if manifold is not None:
+        raise NotImplementedError
+    else:
+        k2 = odefunc(t0 + dt / 3, xt + dt * k1 / 3)
+        k3 = odefunc(t0 + dt * 2 / 3, xt + dt * (k2 - k1 / 3))
+        k4 = odefunc(t0 + dt, xt + dt * (k1 - k2 + k3))
+        return xt + (k1 + 3 * (k2 + k3) + k4) * dt * 0.125
+    
 
 def heun_step(odefunc, xt, vt, t, dt, manifold=None):
     """Heun's method.""" # TODO copilot generated, get actual code from Malte
